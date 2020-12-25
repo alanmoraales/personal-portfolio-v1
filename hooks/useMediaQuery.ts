20d2ba92c2015243fe0b23b7e089903acedbea94
Alan Morales <alan_jesus99@hotmail.com>
2020-12-25 10:53:08 -0600

    added useMediaQuery hook

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { useMediaQuery as chakraUseMediaQuery } from "@chakra-ui/react";
+
+// original chakra ui hook causes problems with nextjs at first load.
+// this wrapper solves the problem
+export const useMediaQuery = (mediaQuery: string | string[]) => {
+  const [mediaState, setMediaState] = useState(false);
+  const [chakraMediaState] = chakraUseMediaQuery(mediaQuery);
+
+  useEffect(() => {
+    if (chakraMediaState) {
+      setMediaState(true);
+    } else {
+      setMediaState(false);
+    }
+  }, [chakraMediaState]);
+
+  return [mediaState];
+};
