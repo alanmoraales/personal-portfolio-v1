f5bca76899b964809a1b847b05694bf6e86a45b2
Alan Morales <alan_jesus99@hotmail.com>
2020-12-25 10:54:08 -0600

    moving head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import defaultTheme from "../themes/defaultTheme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS={true} theme={defaultTheme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>alanmoraales / dev</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
+          rel="stylesheet"
+        ></link>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+      <ChakraProvider resetCSS={true} theme={defaultTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
