47e92c53ec40a3424e0da19449595cb609d38e2f
Alan Morales <alan_jesus99@hotmail.com>
2020-12-26 17:57:23 -0600

    added projects data for static pages

---
+++ data/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  name: string;
+  shortDescription: string;
+  fullDescription: string;
+  repositoryURL: string;
+  liveSiteURL: string;
+  slug: string;
+  coverURL: string;
+}
---
+++ data/projects.ts
@@ -0,0 +1,15 @@
+import { Project } from "./Project";
+
+export const projects: Project[] = [
+  {
+    name: "Open Spotify Clone",
+    shortDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At a, habitasse egestas nunc, mollis. Tincidunt consectetur vitae.",
+    fullDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At a, habitasse egestas nunc, mollis. Tincidunt consectetur vitae.",
+    repositoryURL: "https://github.com/alanmoraales/spotify-api-client",
+    liveSiteURL: "https://spotify-api-client.vercel.app/",
+    slug: "open-spotify-clone",
+    coverURL: "/images/projects/open-spotify-clone.png",
+  },
+];
