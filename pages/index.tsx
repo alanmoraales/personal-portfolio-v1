4dc76f84b5027d62d726cf97f631d72589addc29
Alan Morales <alan_jesus99@hotmail.com>
2020-12-26 23:30:18 -0600

    added about me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from "next";
-import { Flex, Box, Heading } from "@chakra-ui/react";
+import { Flex, Box, Heading, Stack, Text } from "@chakra-ui/react";
 import { Menu } from "../components/organisms/Menu";
 import { Hero } from "../components/layout/Hero";
 import { Projects } from "../components/layout/Projects";
@@ -25,7 +25,31 @@ const Index = ({ projects }: { projects: Project[] }) => {
             <Projects projects={projects} />
           </section>
         </main>
-        <Box h="100vh" />
+        <section id="about me">
+          <Stack
+            pt="65px"
+            pb="65px"
+            spacing="65px"
+            mt="65px"
+            bg="primary.500"
+            color="white"
+            align="center"
+          >
+            <Heading size="sm" textAlign="center">
+              about me
+            </Heading>
+            <Box width="80%" maxWidth="500px">
+              <Text fontSize="2xl" textAlign="center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Placerat varius interdum neque, sit fermentum interdum feugiat.
+                Mattis suspendisse tellus lobortis eleifend bibendum quisque
+                odio malesuada. Bibendum risus, odio scelerisque odio viverra.
+                Ornare volutpat posuere cursus diam at elit.
+              </Text>
+            </Box>
+          </Stack>
+        </section>
+        <Box h="30vh" />
       </Flex>
     </>
   );
