df7313e7e8adb515c8c396a4fab17be808e2a186
Alan Morales <alan_jesus99@hotmail.com>
2020-12-29 01:15:15 -0600

    finished single project view

--- pages/projects/[name].tsx
+++ pages/projects/[name].tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import Link from "next/link";
 import { GetStaticPaths, GetStaticProps } from "next";
 import {
   Box,
@@ -9,12 +8,15 @@ import {
   Heading,
   Image,
   Grid,
-  Link as ChakraLink,
   Text,
   Badge,
   Spacer,
 } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
 import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "../../components/atoms/Link";
+import { useMediaQuery } from "../../hooks";
 import path from "path";
 import fs from "fs";
 import { IndexPage, Project } from "../../data";
@@ -25,62 +27,87 @@ const ProjectPage: FC<Project> = ({
   liveSiteURL,
   fullDescription,
   coverURL,
+  technologies,
 }) => {
+  const [useDesktopLayout] = useMediaQuery("(min-width: 1000px)");
+
   return (
     <>
       <Box width="100%" height="30vh" maxHeight="300px">
         <Image src={coverURL} width="100%" height="100%" objectFit="cover" />
       </Box>
-      <Flex width="100%" justify="center">
-        <Stack width="80%" maxWidth="1150px" p="65px 0px" spacing="65px">
-          <Stack spacing="30px">
-            <Stack spacing="7px">
-              <Heading size="lg" pr="15px">
-                {name}
-              </Heading>
-              <Link href="/#projects" passHref>
-                <ChakraLink>
-                  <Stack
-                    spacing="5px"
-                    align="center"
-                    direction="row"
-                    width="100%"
-                  >
-                    <BsArrowLeft />
-                    <span>go back</span>
-                  </Stack>
-                </ChakraLink>
-              </Link>
-            </Stack>
-            <Grid templateColumns="1fr 10px 60%">
-              <div>
-                <Button variant="outline" width="100%">
-                  repo
-                </Button>
-              </div>
-              <Spacer />
-              <div>
-                <Button
-                  variant="solid"
-                  colorScheme="purple"
-                  width="100%"
-                  minWidth="0px"
-                >
-                  see live
-                </Button>
-              </div>
-            </Grid>
+      <Stack width="100%" align="center" pt="25px">
+        <Grid
+          templateRows={useDesktopLayout ? "none" : "auto 30px auto"}
+          templateColumns={useDesktopLayout ? "60% 30px 1fr" : "none"}
+          position="sticky"
+          top="0px"
+          width="80%"
+          maxWidth="1150px"
+          bg="rgba(255, 255, 255, 0.8)"
+          zIndex="5"
+          pt="40px"
+          pb="30px"
+          style={{ backdropFilter: "blur(5px)" }}
+        >
+          <Stack spacing="7px">
+            <Heading size="lg" pr="15px">
+              {name}
+            </Heading>
+            <Link href="/#projects">
+              <Stack spacing="5px" align="center" direction="row" width="100%">
+                <BsArrowLeft />
+                <span>go back</span>
+              </Stack>
+            </Link>
           </Stack>
+          <Spacer />
+          <Grid templateColumns="1fr 10px 60%">
+            <Button
+              variant="outline"
+              isFullWidth
+              as="a"
+              href={repositoryURL}
+              target="blank"
+              rightIcon={<FaGithub />}
+            >
+              repo
+            </Button>
+            <Spacer />
+            <Button
+              variant="solid"
+              colorScheme="purple"
+              isFullWidth
+              as="a"
+              href={liveSiteURL}
+              target="blank"
+              rightIcon={<FiExternalLink />}
+            >
+              see live
+            </Button>
+          </Grid>
+        </Grid>
+        <Stack width="80%" maxWidth="1150px" pb="65px" pt="35px" spacing="65px">
           <Stack spacing="20px">
             <Heading size="sm">Technologies</Heading>
-            <Badge width="fit-content">React</Badge>
+            <Flex wrap="wrap" style={{ gap: "10px" }}>
+              {technologies.map((tec) => (
+                <Badge
+                  width="fit-content"
+                  bg={tec.color ? tec.color : "primary.500"}
+                  key={tec.name}
+                >
+                  {tec.name}
+                </Badge>
+              ))}
+            </Flex>
           </Stack>
           <Stack spacing="20px">
             <Heading size="sm">Description</Heading>
             <Text>{fullDescription}</Text>
           </Stack>
         </Stack>
-      </Flex>
+      </Stack>
     </>
   );
 };
