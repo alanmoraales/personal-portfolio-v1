92dda386504941ebae8d36785f72a948a4a714be
Alan Morales <alanmoraales845@gmail.com>
2021-01-24 23:13:55 -0600

    fix: using useLayoutEffect instead of useEffect

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import { useMediaQuery as chakraUseMediaQuery } from "@chakra-ui/react";
 
 // original chakra ui hook causes problems with nextjs at first load.
@@ -7,7 +7,7 @@ export const useMediaQuery = (mediaQuery: string | string[]) => {
   const [mediaState, setMediaState] = useState(false);
   const [chakraMediaState] = chakraUseMediaQuery(mediaQuery);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (chakraMediaState) {
       setMediaState(true);
     } else {
